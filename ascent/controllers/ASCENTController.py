49a2bf4a0678a9731c80a3e05ada5a8f2ec8d776
Saurav <skkumarsauravkgp@gmail.com>
2023-04-10 11:37:10 -0400

    unique bs id added

--- ascent/controllers/ASCENTController.py
+++ ascent/controllers/ASCENTController.py
@@ -2,6 +2,7 @@ import glob
 import json
 import math
 import os
+import uuid
 import pandas as pd
 
 from settings import settings
@@ -26,6 +27,7 @@ class SIMInput:
 
 
 class ASCENTController:
+    INCLUSION_ZONE_RADIUS = None
     EXCLUSION_ZONE_RADIUS = None
     EXCLUSION_ZONE_RADIUS_STEP = None
     SIMULATION_COUNT = None
@@ -39,6 +41,7 @@ class ASCENTController:
     settings_file = settings.SIM_SETTINGS_FILE
 
     def __init__(self):
+        self.INCLUSION_ZONE_RADIUS = settings.INCLUSION_ZONE_RADIUS
         self.EXCLUSION_ZONE_RADIUS = settings.EXCLUSION_ZONE_RADIUS
         self.EXCLUSION_ZONE_RADIUS_STEP = settings.EXCLUSION_ZONE_RADIUS_STEP
         self.SIMULATION_COUNT = settings.SIMULATION_COUNT
@@ -49,9 +52,9 @@ class ASCENTController:
         self.FSS_COOR = [37.20250, -80.43444]  # Hard-coded
 
         self.rain = True
-        self.rain_rate = 33.0  # FSS location's rain rate in mm/h
+        self.rain_rate = 33.0  # FSS location's rain rate in mm/h -> YES
 
-        self.BASE_STATIONS = self.generate_BS_list(self.BS_COUNT)
+        self.BASE_STATIONS = self.generate_BS_list(33)
 
         sim_settings = self.configure_simulator_settings()
 
@@ -59,7 +62,9 @@ class ASCENTController:
         if sim_sett is None:
             sim_sett = {}
 
-        self.EXCLUSION_ZONE_RADIUS = sim_sett.get("radius", self.EXCLUSION_ZONE_RADIUS)
+        self.INCLUSION_ZONE_RADIUS = sim_sett.get("radius", self.INCLUSION_ZONE_RADIUS)
+        self.EXCLUSION_ZONE_RADIUS = sim_sett.get("exclusion_radius", self.EXCLUSION_ZONE_RADIUS)
+        self.EXCLUSION_ZONE_RADIUS_STEP = sim_sett.get("exclusion_radius_step", self.EXCLUSION_ZONE_RADIUS_STEP)
         self.SIMULATION_COUNT = sim_sett.get("simulation_count", self.SIMULATION_COUNT)
         self.BS_UE_RADIUS_MAX = sim_sett.get("bs_ue_max_radius", self.BS_UE_RADIUS_MAX)
         self.BS_UE_RADIUS_MIN = sim_sett.get("bs_ue_min_radius", self.BS_UE_RADIUS_MIN)
@@ -70,7 +75,9 @@ class ASCENTController:
                 self.BASE_STATIONS = self.generate_BS_list(self.BS_COUNT)
 
         data = {
-            "radius": self.EXCLUSION_ZONE_RADIUS,
+            "radius": self.INCLUSION_ZONE_RADIUS,
+            "exclusion_radius": self.EXCLUSION_ZONE_RADIUS,
+            "exclusion_radius_step": self.EXCLUSION_ZONE_RADIUS_STEP,
             "simulation_count": self.SIMULATION_COUNT,
             "bs_ue_max_radius": self.BS_UE_RADIUS_MAX,
             "bs_ue_min_radius": self.BS_UE_RADIUS_MIN,
@@ -87,7 +94,7 @@ class ASCENTController:
         return self.sim_input.get_input(
             self.FSS_COOR[0],
             self.FSS_COOR[1],
-            self.EXCLUSION_ZONE_RADIUS,
+            self.INCLUSION_ZONE_RADIUS,
             self.SIMULATION_COUNT,
             self.BS_UE_RADIUS_MAX,
             self.BS_UE_RADIUS_MIN,
@@ -102,12 +109,12 @@ class ASCENTController:
         all_files = glob.glob(os.path.join(path, "*.csv"))
 
         all_data = pd.concat((pd.read_csv(
-            f, names=["radio", "mcc", "mnc", "lac", "cid", "unit", "longitude",
-                      "latitude", "range", "samples", "changeable", "created", "updated", "averageSignal"]
-        ) for f in all_files))
+            file, names=["radio", "mcc", "mnc", "lac", "cid", "unit", "longitude",
+                         "latitude", "range", "samples", "changeable", "created", "updated", "averageSignal"]
+        ) for file in all_files))
 
-        latitude_range = self.EXCLUSION_ZONE_RADIUS / 110574
-        longitude_range = self.EXCLUSION_ZONE_RADIUS / (111320 * math.cos(math.radians(latitude_range)))
+        latitude_range = self.INCLUSION_ZONE_RADIUS / 110574
+        longitude_range = self.INCLUSION_ZONE_RADIUS / (111320 * math.cos(math.radians(latitude_range)))
 
         data_within_zone = all_data[
             (all_data["radio"] == "GSM")
@@ -115,9 +122,12 @@ class ASCENTController:
             & (all_data["longitude"] >= (self.FSS_COOR[1] - longitude_range))
             & (all_data["latitude"] <= (self.FSS_COOR[0] + latitude_range))
             & (all_data["latitude"] >= (self.FSS_COOR[0] - latitude_range))
-            ]
+        ].copy(deep=True)
 
-        selected_bs = data_within_zone.sample(n=bs_count)
-        selected_bs["status"] = 1
+        if len(data_within_zone) > bs_count:
+            data_within_zone = data_within_zone.sample(n=bs_count)
 
-        return selected_bs
+        data_within_zone["status"] = 1
+        data_within_zone["unique_id"] = [str(uuid.uuid4()) for i in range(len(data_within_zone))]
+
+        return data_within_zone
